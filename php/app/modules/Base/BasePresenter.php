094c400ce948680e7174b8a8fa20bb011bf76fe8
dzelinka <dz@dzelinka>
2017-07-04 15:30:51 +0200

    appUrl to csw

--- php/app/modules/Base/BasePresenter.php
+++ php/app/modules/Base/BasePresenter.php
@@ -25,6 +25,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
         $tmp_appparameters = $this->context->parameters;
         $tmp_appparameters['appDefaultLocale'] = $this->translator->getDefaultLocale();
         $tmp_appparameters['appLocale'] = $this->translator->getLocale();
+        $tmp_appparameters['appUrl'] = $this->link(':Catalog:Search:default');
         
         $this->layoutTheme = $this->context->parameters['app']['layoutTheme'];
         $this->langCodes = $this->context->parameters['langCodes'];
