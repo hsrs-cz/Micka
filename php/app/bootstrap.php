6196b31f90c4038d6dae8dd6a495d62dbd82088a
dzelinka <zelinka@email.cz>
2018-12-06 14:08:48 +0100

    DebugMod off

--- php/app/bootstrap.php
+++ php/app/bootstrap.php
@@ -4,8 +4,8 @@ require __DIR__ . '/model/tmp_micka_lib.php';
 require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->setDebugMode(true); // enable for your remote IP
+//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
+$configurator->setDebugMode(false); // enable for your remote IP
 $configurator->enableTracy(__DIR__ . '/../log');
 
 $configurator->setTimeZone('Europe/Prague');
