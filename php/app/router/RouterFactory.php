dd764ad27b7e4df924ec910050cc3dec8a66f20b
dzelinka <zelinka@email.cz>
2019-06-13 14:22:43 +0200

    fix router

--- php/app/router/RouterFactory.php
+++ php/app/router/RouterFactory.php
@@ -17,7 +17,19 @@ class RouterFactory
 	public static function createRouter(Nette\DI\Container $container)
 	{
         $router = new RouteList;
-
+        $langCodes = array_flip($container->parameters['langCodes']);
+        $appLangs = explode(',', $container->parameters['app']['langs']);
+        $translation = 'en';
+        $moreTranslation = '';
+        foreach ($appLangs as $key => $lang) {
+            if ($key === 0) {
+                $translation = $langCodes[$lang];
+            } else {
+                $moreTranslation .= '|' . $langCodes[$lang];
+            }
+        }
+        $locale = $translation. ' '  . $translation . $moreTranslation;
+        
         if ($container->parameters['consoleMode']) {
             $router[] = new CliRouter(array('action' => 'Cli:Cli:default'));
         } else {
@@ -27,35 +39,33 @@ class RouterFactory
             $validatorRouter[] = new Route('validator/<presenter>/<action>', 'Default:form');
 
             $router[] = $adminRouter = new RouteList('Admin');
-            $adminRouter[] = new Route('[<locale=cs cs|en|es>/]admin/<presenter>/<action>[/<id>]', 'Default:default');
+            $adminRouter[] = new Route('[<locale=' . $locale . '>/]admin/<presenter>/<action>[/<id>]', 'Default:default');
 
             $router[] = new Route('index.php', 'Catalog:Search:default', Route::ONE_WAY);
-            $router[] = new Route('[<locale=cs cs|en|es>/]sign/in', ['module' => 'Catalog', 'presenter' => 'Sign', 'action' => 'in']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]sign/out', ['module' => 'Catalog', 'presenter' => 'Sign', 'action' => 'out']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/basic[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'basic']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/full[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'full']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/xml[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'xml']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/new', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'new']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/edit[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'edit']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/save[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'save']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/valid[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'valid']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/clone[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'clone']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]record/delete[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'delete']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]page[/<id>]', ['module' => 'Catalog', 'presenter' => 'Page', 'action' => 'default']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]suggest/mdcontacts', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdContacts']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]suggest/mdlists', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdLists']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]suggest/metadata', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'Metadata']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]suggest/mdupload', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdUpload']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]suggest/mdgazcli', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdGazcli']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]suggest', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'default']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]csw', ['module' => 'Catalog', 'presenter' => 'Csw', 'action' => 'default']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]csw/filter[/<id>]', ['module' => 'Catalog', 'presenter' => 'Csw', 'action' => 'filter']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]about', ['module' => 'Catalog', 'presenter' => 'Micka', 'action' => 'about']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]help', ['module' => 'Catalog', 'presenter' => 'Micka', 'action' => 'help']);
-            $router[] = new Route('record[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'old']);
-            $router[] = new Route('[<locale=cs cs|en|es>/]<module=Catalog>/<presenter=Default>/<action=default>', 'Catalog:Default:default');
+            $router[] = new Route('[<locale=' . $locale . '>/]sign/in', ['module' => 'Catalog', 'presenter' => 'Sign', 'action' => 'in']);
+            $router[] = new Route('[<locale=' . $locale . '>/]sign/out', ['module' => 'Catalog', 'presenter' => 'Sign', 'action' => 'out']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/basic[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'basic']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/full[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'full']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/xml[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'xml']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/new', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'new']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/edit[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'edit']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/save[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'save']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/valid[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'valid']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/clone[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'clone']);
+            $router[] = new Route('[<locale=' . $locale . '>/]record/delete[/<id>]', ['module' => 'Catalog', 'presenter' => 'Record', 'action' => 'delete']);
+            $router[] = new Route('[<locale=' . $locale . '>/]page[/<id>]', ['module' => 'Catalog', 'presenter' => 'Page', 'action' => 'default']);
+            $router[] = new Route('[<locale=' . $locale . '>/]suggest/mdcontacts', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdContacts']);
+            $router[] = new Route('[<locale=' . $locale . '>/]suggest/mdlists', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdLists']);
+            $router[] = new Route('[<locale=' . $locale . '>/]suggest/metadata', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'Metadata']);
+            $router[] = new Route('[<locale=' . $locale . '>/]suggest/mdupload', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdUpload']);
+            $router[] = new Route('[<locale=' . $locale . '>/]suggest/mdgazcli', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'MdGazcli']);
+            $router[] = new Route('[<locale=' . $locale . '>/]suggest', ['module' => 'Catalog', 'presenter' => 'Suggest', 'action' => 'default']);
+            $router[] = new Route('[<locale=' . $locale . '>/]csw', ['module' => 'Catalog', 'presenter' => 'Csw', 'action' => 'default']);
+            $router[] = new Route('[<locale=' . $locale . '>/]csw/filter[/<id>]', ['module' => 'Catalog', 'presenter' => 'Csw', 'action' => 'filter']);
+            $router[] = new Route('[<locale=' . $locale . '>/]about', ['module' => 'Catalog', 'presenter' => 'Micka', 'action' => 'about']);
+            $router[] = new Route('[<locale=' . $locale . '>/]help', ['module' => 'Catalog', 'presenter' => 'Micka', 'action' => 'help']);
+            $router[] = new Route('[<locale=' . $locale . '>/]<module=Catalog>/<presenter=Default>/<action=default>', 'Catalog:Default:default');
         }
-        
 		return $router;
 	}
 
