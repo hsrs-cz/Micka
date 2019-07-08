84b6a2911733708ae1610568a24ddf4de7d1ea53
dzelinka <zelinka@email.cz>
2019-07-08 15:19:20 +0200

    Small improvements

--- php/app/modules/Admin/DefaultPresenter.php
+++ php/app/modules/Admin/DefaultPresenter.php
@@ -9,16 +9,19 @@ class DefaultPresenter extends \BasePresenter
 	public function startup()
 	{
 		parent::startup();
-
 	}
-
-
-	/********************* view default *********************/
 
     /** @resource Admin */
 	public function renderDefault()
 	{
-	
-	}
-
+        $this->template->addModules = array();
+        if (isset($this->context->parameters['addModules']['admin']) && is_array($this->context->parameters['addModules']['admin'])) {
+            foreach($this->context->parameters['addModules']['admin'] as $key => $row) {
+                $module = explode(':', $row);
+                $this->template->addModules[$key]['module'] = ucfirst($module[0]);
+                $this->template->addModules[$key]['presenter'] = isset($module[1]) ? ucfirst($module[1]) : 'Default';
+                $this->template->addModules[$key]['action'] = isset($module[2]) ? strtolower($module[2]) : 'default';
+            }
+        }
+    }
 }
